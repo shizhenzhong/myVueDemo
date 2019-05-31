da5b7437c9f13b59938ea954a39aa0ad0e9a12b6
shizhenzhong <shizhenzhong2013@qq.com>
2019-05-31 22:08:43 +0800

    实现获取图片列表方法

--- src/app.vue
+++ src/app.vue
@@ -33,25 +33,61 @@
 </script>
 
 <style lang="scss" scoped>
- .app-container{
-     padding-top: 40px;
-		 overflow-x: hidden;
- }
+ .mint-header{
+  z-index: 99;
+}
+.app-container {
+  padding-top: 40px;
+	padding-bottom: 50px;
+  overflow-x: hidden;
+}
 
- .v-enter{
-	 opacity: 0;
-	 transform: translate(100%);
-	 position: absolute;
- }
- .v-leave-to{
-	 opacity: 0;
-	 transform:translate(-100%);
-	 position: absolute;
- }
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
 
- .v-enter-active,
- .v-leave-active{
-   transition: all 0.5s ease;
- }
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+// 该类名，解决 tabbar 点击无法切换的问题
+.mui-bar-tab .mui-tab-item-llb.mui-active {
+    color: #007aff;
+}
+
+.mui-bar-tab .mui-tab-item-llb {
+    display: table-cell;
+    overflow: hidden;
+    width: 1%;
+    height: 50px;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #929292;
+}
+
+.mui-bar-tab .mui-tab-item-llb .mui-icon {
+    top: 3px;
+    width: 24px;
+    height: 24px;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+
+.mui-bar-tab .mui-tab-item-llb .mui-icon~.mui-tab-label {
+    font-size: 11px;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
 </style>
 
--- src/main.js
+++ src/main.js
@@ -6,19 +6,22 @@ Vue.use(VueRouter);//安装路由
 import app from './app.vue'
 import router from './router.js'
 
-import { Header,Swipe, SwipeItem} from 'mint-ui';
+// import { Header,Swipe, SwipeItem} from 'mint-ui';
 import './lib/mui/css/mui.min.css';
 import './lib/mui/css/icons-extra.css';
 import './lib/mui/fonts/mui-icons-extra.ttf'
 
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
+import 'mint-ui/lib/style.css'
 
 import vueResource from 'vue-resource'
 Vue.use(vueResource)
 
 
-Vue.component(Header.name, Header);
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
+// Vue.component(Header.name, Header);
+// Vue.component(Swipe.name, Swipe);
+// Vue.component(SwipeItem.name, SwipeItem);
 
 var vm=new Vue({
     el:"#app",
